6f1dcf869ccb2c5678b952d3b6f734db6cd033a6
robloxbedwarshackersduo <145053198+robloxbedwarshackersduo@users.noreply.github.com>
2025-01-01 21:25:06 +0200

    Update and rename rivals-hacks-go-to-the-other-part.lua to part1.lua

--- part1.lua
+++ part1.lua
@@ -1,4 +1,4 @@
 --there are keys btw
-
+-- GO TO THE PART 2 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 loadstring(game:HttpGet("https://x8-bit.web.app/scripts/rival_c1.lua"))()
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Sheeshablee73/Scriptss/main/RivalsUPD2.lua')))()
